4b1ef7f6de7ade37300bb1e80c57a15e78bd42e0
paopaoyun <1270883952@qq.com>
2020-02-19 23:51:37 +0800

    增加领赏模块，增加nacos服务注册与发现

--- src/main/java/com/color/usercenter/dao/bonus/BonusEventLogMapper.java
+++ src/main/java/com/color/usercenter/dao/bonus/BonusEventLogMapper.java
@@ -1,7 +1,9 @@
 package com.color.usercenter.dao.bonus;
 
 import com.color.usercenter.domain.entity.BonusEventLog;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface BonusEventLogMapper extends Mapper<BonusEventLog> {
 }
--- src/main/java/com/color/usercenter/dao/user/UserMapper.java
+++ src/main/java/com/color/usercenter/dao/user/UserMapper.java
@@ -1,7 +1,9 @@
 package com.color.usercenter.dao.user;
 
 import com.color.usercenter.domain.entity.User;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface UserMapper extends Mapper<User> {
 }
